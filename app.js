63b2bb08868febe27e8cc4291e6ce81794088548
Syedafaqahmad <82577242+Syedafaqahmad@users.noreply.github.com>
2024-02-05 08:06:20 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+// question no 1
+// alert ("welcome to website")
+
+
+
+// question no2 
+// alert("Please Enter Valid Password")
+
+
+// question no3
+// alert("Message 1") 
+// alert("Message 2") 
+// alert("Message 3")
+
+
+
+
+// question no7 
+// var username 
+
+
+// // question no 8 
+// var useName = "Syed Afaq Ahmad"; 
+ 
+
+// Q:10 = Write a script to save student’s bio data in JS variables and show the data in 
+// alert boxes?
+// var studentName = "Syed Afaq Ahmad" 
+// var FatherName = "Syed Iftikhar Ahmad" 
+// var RollNo = 4 
+// var clAss = "BSC" 
+
+// alert(studentName) 
+// alert(FatherName)
+// alert(4) 
+// alert(clAss) 
+
+
+// Q:11 = Declare a variable called email and assign to it a string that represents your 
+// Email Address(e.g. example@example.com). Show the blow mentioned 
+// message in an alert box.(Hint: use string concatenation)?
+// var Email = "Syedafqahmad44@gmail.com" 
+// alert (Email)
+
+
+// Q=12 write a script to display this in browser through js?
+// alert("browser Through js")
+
+
+
+// Q=13 Store following string in a variable and show in alert and browser through js 
+// var cityName = "Karachi"; 
+// alert(cityName)
+
+
+// Q=14 Declare a variable called age & assign to it your age. show your age in an alert box
+// var myAge = "18 years old" 
+// alert (myAge) 
+
+
+// Q=16 Declare a variable called birth year & assign to it your birth year.show the following in your browser 
+// var DOB = "28 january / 2001" 
+// alert(DOB) 
+
+
+// Q=18 Declare 3 variable in one statment 
+// var CountryNAme = "Pakistan , India ,Afghinstan " 
+// alert (CountryNAme) 
+
+// Q=21 
+var a = 10;
+alert("afaq"+a) 
+
